7014839df5af5ed3e3f5590a73e3691adb2ed539
Full Name <achonyerichard@gmail.com>
2023-05-09 17:21:55 +0200

    seconf commit

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
       <header className="bg-[#DD8888] lg:px-16 px-8  flex flex-wrap items-center py-2 md:py-4 shadow-md max-w-[1920px]">
         <div className="flex-1 flex justify-between items-center">
           <a href="#" className="text-xl text-black">
-            DentalPro
+            Nile-Shop
           </a>
         </div>
 
