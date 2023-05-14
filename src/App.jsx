526a9864de86f7b463067ef268f2743aa9bf316e
Full Name <achonyerichard@gmail.com>
2023-05-14 12:30:39 +0200

    reviews

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import ProductDetails from './pages/Home/ProductDetails'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
     </>
   )
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,44 @@
-import styles from "./Login.module.css"
+import {Link} from "react-router-dom"
 
 const Login = () => {
   return (
     <>
-    <div className="flex justify-center items-center h-screen bg-gray-200">
-    <div className={`${styles.box}`}>
-        <div className={`${styles.form} shadow-xl`}>
-          <h2>SIGN IN</h2>
-          <form>
+   <div className="login-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" >
+    <header className="max-w-lg mx-auto">
+        <a href="#">
+            <h1 className="text-4xl font-bold text-white text-center">Nile-Shop</h1>
+        </a>
+    </header>
 
-          
-          <div className={`${styles.inputBox}`}>
-            <input type="text" required />
-            <span>Username</span>
-            <i></i>
-          </div>
-          <div className={`${styles.inputBox}`}>
-            <input type="password" required />
-            <span>Password</span>
-            <i></i>
-          </div>
-          <div className={`${styles.links}`}>
-            <a href="#">Forget password</a>
-            <a href="#">Signup</a>
-          </div>
-          <button className="rounded-lg px-4 py-2 bg-[#975720] text-green-100 hover:bg-[#d19058] duration-300">Login</button>
-          </form>
-        </div>
-      </div>
+    <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <section>
+        <h3 className="font-bold text-2xl text-black">Login</h3>
+            <p className="text-gray-600 pt-2">Sign up with your email and password</p>
+        </section>
+
+        <section className="mt-10">
+            <form className="flex flex-col" method="POST" action="#">
+                <div className="mb-6 pt-3 rounded bg-gray-200">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" htmlFor="email">Input your email</label>
+                    <input type="text" id="email" className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-[#A16161] transition duration-500 px-3 "/>
+                </div>
+                <div className="mb-6 pt-3 rounded bg-gray-200">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" htmlFor="password">Register</label>
+                    <input type="password" id="password" className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-[#A16161] transition duration-500 px-3 pb-3"/>
+                </div>
+                
+           
+                <button className="bg-[#A16161] hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Login</button>
+            </form>
+        </section>
+    </main>
+
+    <div className="max-w-lg mx-auto text-center mt-12 mb-6">
+        <p className="text-white">Dont have an account ? <Link to="/register" className="font-bold hover:underline">Register</Link>.</p>
     </div>
+
+   
+</div>
   
     </>
   );
