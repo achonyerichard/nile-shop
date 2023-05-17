4b2fc84a057929ff2f71e70c9c9533c5763fe2ff
Full Name <achonyerichard@gmail.com>
2023-05-17 15:36:55 +0200

    first commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,13 +2,14 @@ const Footer = () => {
     return (
       <>
         {" "}
-        <footer className="px-5 sm:px-10 md:px-20 py-2 bg-[#DD8888]">
+        <div className="relative">
+        <footer className="px-5 sm:px-10 md:px-20 py-2 bg-[#DD8888] bottom-0 sticky w-full">
           <div className="flex  items-center  justify-center">
             <div className="mt-4 text-xs font-medium text-white">
               &copy; 2023 Nile University Project
             </div>
           </div>
-        </footer>
+        </footer></div>
       </>
     );
   };
